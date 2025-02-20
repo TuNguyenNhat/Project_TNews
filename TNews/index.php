f85eb284bd6307258d2ba3918dd27e86db9fddba
TuNguyenNhat <nguyennhattu2905@gmail.com>
2025-02-21 01:10:33 +0700

    Hoàn thiện phần Footer trang index

---
+++ TNews/index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TNews</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./font/icon/fontawesome-free-6.4.2-web/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+
+</head>
+<body>
+    <div class="contair">
+        <!-- Phan dau cua trang -->
+        <header>
+            <div class="logo">
+                <img src="./image/LogoTNews.png" alt="" width="120px">
+                <div class="nav">
+                    <a href="">Trang Chủ</a>
+                    <a href="">Về Chúng Tôi</a>
+                    <a href="">Liên Hệ</a>
+                </div>
+            </div>
+
+            <div class="form_search">
+                <form action="" method="">
+                    <input type="text" name="" id="" placeholder="Tìm kiếm....">
+                    <button type="submit">Tìm kiếm</button>
+                </form>
+            </div>
+            
+            <div class="login">
+                <a href="">Đăng nhập</a>
+            </div>
+        </header>
+
+        <!-- Phan noi dung cua trang -->
+        <div class="content">
+            <!-- Phan menu Danh muc va The loai cua trang -->
+            <div class="content_left">
+                <h3>Danh mục</h3>
+                <!-- Danh muc cua trang -->
+                <ul>
+                    <li>
+                        <a href="">Thời sự</a>
+                        <!-- The loai cua trang -->
+                        <ul>
+                            <li><a href="">Chính trị</a></li>
+                            <li><a href="">Giao thông</a></li>
+                            <li><a href="">Ngoài nước</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">Kinh doanh</a>
+                        <ul>
+                            <li><a href="">Quốc tế</a></li>
+                            <li><a href="">Bất động sản</a></li>
+                            <li><a href="">Doanh nghiệp</a></li>
+                            <li><a href="">Chứng khoán</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">Công nghệ</a>
+                        <ul>
+                            <li><a href="">AI</a></li>
+                            <li><a href="">Thiết bị</a></li>
+                            <li><a href="">Ứng dụng</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">Thể thao</a>
+                        <ul>
+                            <li><a href="">Bóng đá</a></li>
+                            <li><a href="">Bóng rổ</a></li>
+                            <li><a href="">Pickleball</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">Giải trí</a>
+                        <ul>
+                            <li><a href="">Giới sao</a></li>
+                            <li><a href="">Phim</a></li>
+                            <li><a href="">Trending</a></li>
+                            <li><a href="">Thời trang</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <!-- Phan noi dung bai tin cua trang -->
+            <div class="content_center">
+                <h3>Tin đặc biệt của hôm nay</h3>
+                <!-- Phan tin noi bat -->
+                <div class="hot_news">
+                    <!-- anh cua tin hot -->
+                    <div class="image">
+
+                    </div>
+                    <!-- tieu de + noi dng cua tin hot -->
+                    <div class="news_content">
+
+                    </div>
+                </div>
+                <!-- Danh sach cac loai tin -->
+                <div class="lits_news">
+
+                </div>
+            </div>
+        </div>
+
+        <footer>
+            <hr style="color: #014471;">
+            <div class="footer_top">
+                <!-- phan lien he footer -->
+                <div class="top_1">
+                    <div class="social_my">
+                        <div class="social_my-p">
+                            <p>Kết nối với tôi trên mạng xã hội:</p>
+                        </div>
+                        <a href="https://www.facebook.com/nghuyennhattu" target="_blank"><i class="fab fa-facebook"></i></a>
+                        <a href="https://www.instagram.com/402_uttahnn?fbclid=IwAR2ES3b-_ijkS6odu2qZJfYAd1d4N4jlF8djRgwxVCnoleK6Ne4g36lem14" target="_blank"><i class="fab fa-instagram"></i></i></a>
+                        <a href="https://www.tiktok.com/@_nhattu_?_t=8hghxhvdBVX&_r=1" target="_blank"><i class="fab fa-tiktok"></i></a>
+                        <a href="https://github.com/TuNguyenNhat" target="_blank"><i class="fab fa-github"></i></a>
+                    </div>
+                    <div class="contact_my">
+                        <div>
+                            <p>Liên hệ với tôi:</p>
+                        </div>
+                        <div class="pp">
+                            <div>
+                                <i class="fas fa-envelope"></i>
+                                <p>nguyennhattu2905@gmail.com</p>
+                            </div>
+                            <div>
+                                <i class="fas fa-phone-alt"></i>
+                                <p>0393804887</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- phan noi dung cua footer -->
+                <div class="top_2">
+                    <p>Tại đây mọi người có thể nắm bắt tin 
+                            tức và xu hướng một cách dễ dàng 
+                            với một vài thao tác. Không chỉ dừng lại ở đây trang 
+                            Web TNews sẽ tiếp nhận tin tức từ phía của mọi người, 
+                            mong mọi người liên hệ lại với trang
+                    </p>
+                </div>
+                <!-- phan logo footer -->
+                <div class="top_3" style="text-align: center;">
+                    <img src="./image/LogoTNews.png" alt="" width="80px">
+                </div>
+            </div>
+            <div class="footer_bottom">
+                <p>© 2025 Copyright By: Nguyễn Nhật Tú</p>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
